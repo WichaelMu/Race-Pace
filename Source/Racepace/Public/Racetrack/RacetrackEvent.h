5597ad5a03d5950018d9d2cf65bbd26b162c8360
Michael Wu <69779024+WichaelMu@users.noreply.github.com>
2023-06-07 16:18:57 +1000

    Racetrack Events Framework

---
+++ Source/Racepace/Public/Racetrack/RacetrackEvent.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RacetrackEvent.generated.h"
+
+class ARacecar;
+class UBoxComponent;
+
+#define OVERRIDE_ENTER_FUNCTION() UFUNCTION(BlueprintCallable) \
+	virtual void Enter(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override; \
+
+UCLASS()
+class RACEPACE_API ARacetrackEvent : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARacetrackEvent();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+protected:
+
+	UFUNCTION(BlueprintCallable)
+		virtual void Enter(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	FORCEINLINE const ARacecar* CastToRacecar(const AActor* OtherActor) const;
+
+protected:
+
+	UPROPERTY(EditAnywhere)
+		UBoxComponent* Collider;
+};
